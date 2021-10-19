34108ad554a9e371213ea7f7b62e87ff23f96b62
Marius Jagminas <asmarenas@gmail.com>
2021-10-19 15:05:30 +0200

    Setup initial bot

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
+import { Client, Intents, Message } from "discord.js";
+
 void (() => {
-  console.log("Service is running");
+  const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+  });
+
+  client.once("ready", () => {
+    console.log("Bot is ready");
+  });
+
+  client.on("messageCreate", (message: Message) => {
+    console.log("messageCreate", message);
+  });
+
+  client.login("token");
 })();
